0441df7c25adc9f52b682bacbb4d5ed2a62a4a10
Suxian <gsx@suxians-mbp.lan>
2021-11-20 20:28:14 +0800

    add app.js server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+// app.get('/', (req, res) => {
+//  res.send(index.html)
+// })
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
